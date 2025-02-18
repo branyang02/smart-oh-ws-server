3ccafbb27f47502644afa0e07d54f5d2d4caabfb
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2025-02-18 00:51:58 -0500

    updated WS to be just state transfer (no logic is written)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import uuid
+import json
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 
-from websocket_manager import OfficeHourManager, Session, Student, TA
+from websocket_manager import OfficeHourManager, TBoard
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -18,194 +18,28 @@ app.add_middleware(
 manager = OfficeHourManager()
 
 
-@app.websocket("/ws/{class_id}/{user_id}")
-async def office_hour_websocket(
-    websocket: WebSocket, class_id: str, user_id: str, user_type: str, name: str
-):
-    room = manager.get_or_create_room(class_id)
+@app.websocket("/ws/{class_id}")
+async def websocket_endpoint(websocket: WebSocket, class_id: str):
     await websocket.accept()
-    room.connections[user_id] = websocket
+
+    manager.add_connection(class_id, websocket)
+
+    board = manager.get_or_create_room(class_id)
+    await websocket.send_text(board.model_dump_json())
 
     try:
-        if user_type == "TA":
-            # Restore or create TA state
-            if user_id not in room.tas:
-                room.tas[user_id] = TA(user_id, name)
-        elif user_type == "student":
-            # Restore or create student state
-            if user_id not in room.students:
-                room.students[user_id] = Student(user_id, name)
-
-        await room.broadcast_state()
-
         while True:
-            data = await websocket.receive_json()
-            action = data["action"]
+            msg = await websocket.receive_text()
 
-            if action == "join_queue" and user_type == "student":
-                """
-                - {action: "join_queue"}
-                """
-                assert user_id in room.students
-                student = room.students[user_id]
-                assert student.current_location is None
-                room.queue.add_student(student)
+            try:
+                new_state = TBoard.model_validate_json(msg)
+                manager.rooms[class_id] = new_state
+                await manager.broadcast(class_id, new_state.json())
 
-            elif action == "leave_queue" and user_type == "student":
-                """
-                - {action: "leave_queue"}
-                """
-                assert user_id in room.students
-                assert user_id in room.queue.users_dict
-                student = room.students[user_id]
-                assert student.current_location == "queue"
-                room.queue.remove_student(student)
-
-            elif action == "create_session_with_id" and user_type == "TA":
-                """
-                - {action: "create_session_with_id", new_session_id: "session-..."}
-                """
-                new_session_id = data.get("new_session_id")
-                assert new_session_id not in room.sessions
-                assert user_id in room.tas
-                ta = room.tas[user_id]
-                assert new_session_id not in room.sessions
-                new_session = Session(new_session_id)
-                room.sessions[new_session_id] = new_session
-
-                if ta.current_location is not None:
-                    prev_session = room.sessions[ta.current_location]
-                    prev_session.remove_ta(ta)
-                    if len(prev_session.ta_ids) == 0:
-                        del room.sessions[prev_session.id]
-
-                new_session.add_ta(ta)
-
-            elif action == "create_session" and user_type == "TA":
-                """
-                TA creates and joins a new session, leaving the previous one if they were in one
-                - {action: "create_session"}
-                """
-                assert user_id in room.tas
-                ta = room.tas[user_id]
-                new_session_id = f"session-{uuid.uuid4()}"
-                assert new_session_id not in room.sessions
-                new_session = Session(new_session_id)
-                room.sessions[new_session_id] = new_session
-
-                if ta.current_location is not None:
-                    prev_session = room.sessions[ta.current_location]
-                    prev_session.remove_ta(ta)
-                    if len(prev_session.ta_ids) == 0:
-                        del room.sessions[prev_session.id]
-
-                new_session.add_ta(ta)
-
-            elif action == "join_session" and user_type == "TA":
-                """
-                TA joins a new session, leaving the previous one if they were in one
-                - {action: "join_session", session_id: "ta1", index: 0}
-                """
-                session_id = data.get("session_id")
-                index = data.get("index")
-                assert session_id in room.sessions
-                assert user_id in room.tas
-                ta = room.tas[user_id]
-
-                if ta.current_location is not None:
-                    prev_session = room.sessions[ta.current_location]
-                    prev_session.remove_ta(ta)
-                    if len(prev_session.ta_ids) == 0:
-                        del room.sessions[prev_session.id]
-
-                room.sessions[session_id].add_ta(ta, index)
-
-            elif action == "leave_session" and user_type == "TA":
-                """
-                This is used when a TA wants to leave a session completely. If a TA wants to switch session, they can call join_session instead.
-                {action: "leave_session"}
-                """
-                assert user_id in room.tas
-                ta = room.tas[user_id]
-                assert ta.current_location is not None
-                session = room.sessions[ta.current_location]
-                session.remove_ta(ta)
-                # If the session is empty, remove it
-                if len(session.ta_ids) == 0:
-                    del room.sessions[session.id]
-
-            elif action == "assign_student_to_session" and user_type == "TA":
-                """
-                {action: "assign_student_to_session", student_id: "student1", session_id: "session-...", index: 0}
-                """
-                assert user_id in room.tas
-                student_id = data.get("student_id")
-                session_id = data.get("session_id")
-                index = data.get("index")
-                assert student_id in room.students
-                assert session_id in room.sessions
-                ta = room.tas[user_id]
-                student = room.students[student_id]
-                session = room.sessions[session_id]
-
-                assert student.current_location is not None
-
-                if student.current_location == "queue":
-                    assert student_id in room.queue.users_dict
-                    room.queue.remove_student(student)
-                elif student.current_location in room.sessions:
-                    prev_session = room.sessions[student.current_location]
-                    prev_session.remove_student(student)
-
-                session.add_student(student, index)
-
-            elif action == "assign_student_to_queue" and user_type == "TA":
-                """
-                {action: "assign_student_to_queue", student_id: "student1", index: 0}
-                """
-                assert user_id in room.tas
-                index = data.get("index")
-                student_id = data.get("student_id")
-                assert student_id in room.students
-                ta = room.tas[user_id]
-                student = room.students[student_id]
-                assert student.current_location is not None
-
-                if student.current_location == "queue":
-                    assert student_id in room.queue.users_dict
-                    room.queue.remove_student(student)
-                elif student.current_location in room.sessions:
-                    prev_session = room.sessions[student.current_location]
-                    prev_session.remove_student(student)
-
-                room.queue.add_student(student, index)
-
-            await room.broadcast_state()
-            print("Room: ", room.class_id)
-            print("Connections:", room.connections.keys())
-            print("TAs:", room.tas.keys())
-            print("Students:", room.students.keys())
-            print("Queue:", room.queue.users_list)
-            print(
-                "Sessions:", [session.users_list for session in room.sessions.values()]
-            )
-            print("--" * 30)
+            except Exception as e:
+                error_message = {"error": str(e)}
+                await websocket.send_text(json.dumps(error_message))
 
     except WebSocketDisconnect:
-        """
-        - Fully disconnect TA if they are not in a session.
-        - Fully disconnect student if they are not in the queue or a session.
-        """
-        if user_id in room.tas:
-            ta = room.tas[user_id]
-            if ta.current_location is None:
-                del room.tas[user_id]
-        elif user_id in room.students:
-            student = room.students[user_id]
-            if student.current_location is None:
-                del room.students[user_id]
-
-        if user_id in room.connections:
-            del room.connections[user_id]
-        if room.connections:
-            await room.broadcast_state()
+        # Client disconnected, remove from manager
+        manager.remove_connection(class_id, websocket)
--- websocket_manager.py
+++ websocket_manager.py
@@ -1,220 +1,54 @@
-import uuid
-from fastapi import WebSocket, WebSocketDisconnect
-from typing import Literal, TypedDict, Union
+from fastapi import WebSocket
 from typing import Dict, List
-from collections import OrderedDict
 
-type User = Union[Student, TA]
+from pydantic import BaseModel
 
 
-class UserState(TypedDict):
+# Match Frontend
+class TCard(BaseModel):
     id: str
-    columnId: str  # Session ID | "queue" | "none"
     name: str
-    type: Literal["student", "TA"]
+    type: str
 
 
-class ColumnState(TypedDict):
+class TColumn(BaseModel):
     id: str
     title: str
+    cards: List[TCard]
 
 
-class RoomState(TypedDict):
+class TBoard(BaseModel):
     classId: str
-    allUsers: List[UserState]
-    users: List[UserState]
-    columns: List[ColumnState]
-
-
-class Student:
-    def __init__(self, id: str, name: str):
-        self.id = id
-        self.name = name
-        self.current_location: str = None  # Session ID | "queue"
-
-
-class TA:
-    def __init__(self, id: str, name: str):
-        self.id = id
-        self.name = name
-        self.current_location: str = None  # Session ID
-
-
-class Queue:
-    def __init__(self):
-        self.users_list: List[str] = []  # Student IDs
-        self.users_dict: Dict[str, Student] = {}  # Student ID -> Student
-
-    def add_student(self, student: Student, index: int = None) -> None:
-        if student.id in self.users_dict:
-            raise ValueError("Student is already in the queue")
-
-        student.current_location = "queue"
-        if index is None or index >= len(self.users_list):
-            self.users_list.append(student.id)
-        else:
-            self.users_list.insert(index, student.id)
-        self.users_dict[student.id] = student
-
-    def remove_student(self, student: Student) -> None:
-        if student.id not in self.users_dict:
-            raise ValueError("Student is not in the queue")
-
-        self.users_list.remove(student.id)
-        del self.users_dict[student.id]
-        student.current_location = None
-
-    @property
-    def users_state(self) -> List[UserState]:
-        return [
-            {
-                "id": user_id,
-                "columnId": "queue",
-                "name": self.users_dict[user_id].name,
-                "type": "student",
-            }
-            for user_id in self.users_list
-        ]
-
-
-class Session:
-    def __init__(self, id: str = None):
-        self.id = id
-        self.users_list: List[str] = []  # User IDs
-        self.users_dict: Dict[str, User] = {}  # User ID -> User
-
-    def add_ta(self, ta: TA, index: int = None) -> None:
-        if ta.id in self.users_dict:
-            raise ValueError("TA is already in the session")
-
-        ta.current_location = self.id
-        if index is None or index >= len(self.users_list):
-            self.users_list.append(ta.id)
-        else:
-            self.users_list.insert(index, ta.id)
-        self.users_dict[ta.id] = ta
-
-    def remove_ta(self, ta: TA) -> None:
-        if ta.id not in self.users_dict:
-            raise ValueError("TA is not in the session")
-
-        self.users_list.remove(ta.id)
-        del self.users_dict[ta.id]
-        ta.current_location = None
-
-    def add_student(self, student: Student, index: int = None) -> None:
-        if student.id in self.users_dict:
-            raise ValueError("Student is already in the session")
-
-        student.current_location = self.id
-        if index is None or index >= len(self.users_list):
-            self.users_list.append(student.id)
-        else:
-            self.users_list.insert(index, student.id)
-        self.users_dict[student.id] = student
-
-    def remove_student(self, student: Student) -> None:
-        if student.id not in self.users_dict:
-            raise ValueError("Student is not in the session")
-
-        self.users_list.remove(student.id)
-        del self.users_dict[student.id]
-        student.current_location = None
-
-    @property
-    def student_ids(self) -> List[str]:
-        """Get the IDs of all students in the session"""
-        return [
-            user.id for user in self.users_dict.values() if isinstance(user, Student)
-        ]
-
-    @property
-    def ta_ids(self) -> List[str]:
-        """Get the IDs of all TAs in the session"""
-        return [user.id for user in self.users_dict.values() if isinstance(user, TA)]
-
-    @property
-    def users_state(self) -> List[UserState]:
-        return [
-            {
-                "id": user_id,
-                "columnId": self.id,
-                "name": self.users_dict[user_id].name,
-                "type": "TA" if isinstance(self.users_dict[user_id], TA) else "student",
-            }
-            for user_id in self.users_list
-        ]
-
-
-class OfficeHourRoom:
-    def __init__(self, class_id: str):
-        self.connections: Dict[str, WebSocket] = {}  # user ID -> WebSocket
-
-        self.class_id = class_id  # Class ID
-        self.students: Dict[str, Student] = {}  # Student ID -> Student
-        self.tas: Dict[str, TA] = {}  # TA ID -> TA
-
-        self.queue = Queue()
-        self.sessions: OrderedDict[str, Session] = {}  # Session ID -> Session
-
-    @property
-    def users_state(self) -> List[UserState]:
-        return [
-            {
-                "id": student.id,
-                "columnId": "none",
-                "name": student.name,
-                "type": "student",
-            }
-            for student in self.students.values()
-        ] + [
-            {"id": ta.id, "columnId": "none", "name": ta.name, "type": "TA"}
-            for ta in self.tas.values()
-        ]
-
-    @property
-    def queue_users_state(self) -> List[UserState]:
-        return self.queue.users_state
-
-    @property
-    def session_users_state(self) -> List[UserState]:
-        return sum([session.users_state for session in self.sessions.values()], [])
-
-    async def broadcast_state(self):
-        """Broadcast current state to all connected users. Consistent with frontend types."""
-        state: RoomState = {
-            "classId": self.class_id,
-            "allUsers": self.users_state,
-            "users": self.queue_users_state + self.session_users_state,
-            "columns": [
-                {
-                    "id": "queue",
-                    "title": "Queue",
-                }
-            ]
-            + [
-                {
-                    "id": session.id,
-                    "title": session.id,
-                }
-                for session in self.sessions.values()
-            ],
-        }
-
-        connections = dict(self.connections)
-        for user_id, connection in connections.items():
-            try:
-                await connection.send_json(state)
-            except WebSocketDisconnect:
-                if user_id in self.connections:
-                    del self.connections[user_id]
+    allUsers: List[TCard]
+    columns: List[TColumn]
 
 
 class OfficeHourManager:
     def __init__(self):
-        self.rooms: Dict[str, OfficeHourRoom] = {}  # Class ID -> OfficeHourRoom
+        # class_id -> TBoard
+        self.rooms: Dict[str, TBoard] = {}
 
-    def get_or_create_room(self, class_id: str) -> OfficeHourRoom:
+        # class_id -> list of active WebSocket connections
+        self.connections: Dict[str, List[WebSocket]] = {}
+
+    def get_or_create_room(self, class_id: str) -> TBoard:
         if class_id not in self.rooms:
-            self.rooms[class_id] = OfficeHourRoom(class_id)
+            self.rooms[class_id] = TBoard(classId=class_id, allUsers=[], columns=[])
         return self.rooms[class_id]
+
+    def add_connection(self, class_id: str, websocket: WebSocket) -> None:
+        if class_id not in self.connections:
+            self.connections[class_id] = []
+        self.connections[class_id].append(websocket)
+
+    def remove_connection(self, class_id: str, websocket: WebSocket) -> None:
+        if class_id in self.connections:
+            self.connections[class_id].remove(websocket)
+            if not self.connections[class_id]:
+                del self.connections[class_id]
+
+    async def broadcast(self, class_id: str, data: str) -> None:
+        """Send `data` to all clients connected to a particular class."""
+        if class_id in self.connections:
+            for connection in self.connections[class_id]:
+                await connection.send_text(data)
